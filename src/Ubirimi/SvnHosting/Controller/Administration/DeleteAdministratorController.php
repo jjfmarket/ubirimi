b357102f1d24a33ff757ced5d01adc6ca503f924
flavius_nopcea <domnuprofesor@gmail.com>
2015-05-04 17:26:06 +0300

    increase copyright year

--- src/Ubirimi/SvnHosting/Controller/Administration/DeleteAdministratorController.php
+++ src/Ubirimi/SvnHosting/Controller/Administration/DeleteAdministratorController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- *  Copyright (C) 2012-2014 SC Ubirimi SRL <info-copyright@ubirimi.com>
+ *  Copyright (C) 2012-2015 SC Ubirimi SRL <info-copyright@ubirimi.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 2 as
