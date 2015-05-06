eb5f7514e2f5805113fba29027db7a71add2f677
flavius_nopcea <domnuprofesor@gmail.com>
2015-05-06 13:00:37 +0300

    auth api

--- src/Ubirimi/Api/Controller/Issue/SearchController.php
+++ src/Ubirimi/Api/Controller/Issue/SearchController.php
@@ -20,20 +20,21 @@
 namespace Ubirimi\Api\Controller\Issue;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-
-
+use Symfony\Component\HttpFoundation\Request;
+use Ubirimi\Container\UbirimiContainer;
+use Ubirimi\Repository\User\UbirimiUser;
 use Ubirimi\UbirimiController;
 use Ubirimi\Util;
 use Ubirimi\Yongo\Repository\Issue\Issue;
 
 class SearchController extends UbirimiController
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        UbirimiContainer::get()['api.auth']->auth($request);
+
         $issuesResult = array();
-
         $getSearchParameters = $this->getRepository(Issue::class)->prepareDataForSearchFromURL($_GET, null);
-
         $getSearchParameters['page'] = null;
 
         $parseURLData = parse_url($_SERVER['REQUEST_URI']);
