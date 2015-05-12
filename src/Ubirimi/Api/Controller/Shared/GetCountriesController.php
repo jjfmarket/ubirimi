65430c7c7cca0a8172d6cfa2b9a8acc3cf98a4b0
flavius_nopcea <domnuprofesor@gmail.com>
2015-05-12 09:38:18 +0300

    get countries api

---
+++ src/Ubirimi/Api/Controller/Shared/GetCountriesController.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ *  Copyright (C) 2012-2015 SC Ubirimi SRL <info-copyright@ubirimi.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+namespace Ubirimi\Api\Controller\Client;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Ubirimi\Container\UbirimiContainer;
+use Ubirimi\UbirimiController;
+use Ubirimi\Util;
+
+class GetCountriesController extends UbirimiController
+{
+    public function indexAction(Request $request)
+    {
+        $countriesArray = array();
+        $countriesResult = Util::getCountries();
+        while ($country = $countriesResult->fetch_array(MYSQLI_ASSOC)) {
+            $countriesArray[] = $country;
+        }
+
+        return new JsonResponse($countriesArray);
+    }
+}
