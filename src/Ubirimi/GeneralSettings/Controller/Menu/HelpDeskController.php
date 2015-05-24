d4a871a31f152f757875a9af9708569e2895ebb1
Nopcea Flavius <domnulnopcea@gmail.com>
2015-05-24 15:08:52 +0300

    remove unneeded variables

--- src/Ubirimi/GeneralSettings/Controller/Menu/HelpDeskController.php
+++ src/Ubirimi/GeneralSettings/Controller/Menu/HelpDeskController.php
@@ -29,16 +29,6 @@ class HelpDeskController extends UbirimiController
 {
     public function indexAction(Request $request, SessionInterface $session)
     {
-        if (Util::checkUserIsLoggedIn()) {
-
-            $selectedProjectId = $session->get('selected_project_id');
-        } else {
-            $httpHOST = Util::getHttpHost();
-            $clientId = $this->getRepository(UbirimiClient::class)->getByBaseURL($httpHOST, 'array', 'id');
-            $loggedInUserId = null;
-            $selectedProjectId = null;
-        }
-
         $clientAdministratorFlag = $session->get('user/client_administrator_flag');
 
         return $this->render(__DIR__ . '/../../Resources/views/menu/HelpDesk.php',
